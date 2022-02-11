06e17dccfaf674201405e4e799a9bc2639c6bf25
Konstantin Barkar <BarkarKonstantinRoot@gmail.com>
2022-02-11 17:56:20 +0200

    Create header, add navigation with one item: "Home"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only"></span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
