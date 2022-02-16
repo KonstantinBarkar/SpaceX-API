b6636ff8a9918cd03ce623be2884c914ae873c09
Konstantin Barkar <BarkarKonstantinRoot@gmail.com>
2022-02-16 18:08:44 +0200

    git remote "TEST"

--- src/components/HistoricEvents.jsx
+++ src/components/HistoricEvents.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const HistoricEvents = () => {
   return (
     <div>
-      Здесь будет вывод API
+      {/* 6. Using API History call, show a list of all historic events to the home page 
+      7. Each item of the list should have "title", "details", "event_date_utc" in readable format and a link to the flight (flight_number prop) */}
+      
+
     </div>
   )
 };
