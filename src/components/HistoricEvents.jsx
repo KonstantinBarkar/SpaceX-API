df767361d6385636218c81fb59d653c08d216432
Konstantin Barkar <BarkarKonstantinRoot@gmail.com>
2022-02-16 18:14:32 +0200

    git remote "test 2.0"

--- src/components/HistoricEvents.jsx
+++ src/components/HistoricEvents.jsx
@@ -5,6 +5,8 @@ const HistoricEvents = () => {
     <div>
       {/* 6. Using API History call, show a list of all historic events to the home page 
       7. Each item of the list should have "title", "details", "event_date_utc" in readable format and a link to the flight (flight_number prop) */}
+
+      {/* git remote "test-2" */}
       
 
     </div>
